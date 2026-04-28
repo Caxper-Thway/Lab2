14af0be05844a49762ea3b7aac62035167e41652
Caxper <kaungnyan.25@ichat.sp.edu.sg>
2026-04-28 16:19:16 +0800

    Hello

--- Lab2.py
+++ Lab2.py
@@ -9,6 +9,19 @@ def main():
         print("Normal Weight")
     else:
         print("Over Weight")
+    
+    display_main_menu()
+    num_list = get_user_input()
+
+    average = calc_average_temperature(num_list)
+    print("Average " + str(average))
+ 
+
+    max_min_list = calc_min_max_temperature(num_list)
+
+    print ("Max is " + str(int(max_min_list[0])))
+    print ("Min is " + str(int(max_min_list[1])))
+
 
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
@@ -17,6 +30,43 @@ def calculate_bmi(height, weight):
     bmi = weight/(height * height)
     return round(bmi,2)
 #Add code here to display calculate BMI
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def calc_average():
+    print("calc_average")
+
+def get_user_input():
+    user_input = input()
+    string_list = user_input.split(",")
+    float_list = [];
+    for num_str in string_list:
+        float_list.append(float(num_str))
+    return float_list
+
+def calc_average_temperature(list):
+
+    average = 0
+
+    for num_str in list:
+        average = average + num_str
+
+    return average/5
+
+def calc_min_max_temperature(list):
+    max = 0
+    min = 100
+    for num_str in list:
+        if(num_str > max):
+            max = num_str
+        if(num_str < min):
+            min = num_str
+
+    max_min_list = [max, min];
+
+    return max_min_list
+
+   
 
 
 

67046523ed9017b65e800a7996854823c0d33fb1
Caxper <kaungnyan.25@ichat.sp.edu.sg>
2026-04-28 15:37:11 +0800

    Hello

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,20 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+#Add code here to calculate BMI
+    bmi = weight/(height * height)
+    return round(bmi,2)
+#Add code here to display calculate BMI
+bmi = str(calculate_bmi(weight=57, height=1.73))
+print("BMI =" + bmi)
+
+if float(bmi) < 18.5:
+        print("Under Weight") 
+elif 18.5 <= float(bmi) <= 25.0:
+    print("Normal Weight")
+else:
+    print("Over Weight")
+
+
